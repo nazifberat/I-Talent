b469f8bbbfa47cddaaee26708180de3f37846b03
Rizvida1 <rrab087@uottawa.ca>
2021-05-06 15:01:28 -0400

    axe-core setup

--- services/frontend/src/index.jsx
+++ services/frontend/src/index.jsx
@@ -1,6 +1,13 @@
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import axe from "@axe-core/react";
+// import { render } from "react-dom";
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+if (process.env.NODE_ENV !== "production") {
+  axe(React, ReactDOM, 1000);
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
